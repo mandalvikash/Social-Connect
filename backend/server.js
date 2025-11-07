85365bfe2a346cafd0241394a88941b89b5fe48a
Vikash Kumar Mandal <146217029+mandalvikash@users.noreply.github.com>
2025-11-07 22:28:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: 'https://social-connect-fu3g-hp50ctmkd-vikash-mandals-projects.vercel.app',
+  credentials: true
+}))
 app.use(express.json());
 
 // Routes
